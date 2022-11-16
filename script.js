a3af05dc2f97ab791c75440373f3c012bffb0256
Seidali_ <117403561+Seidali001@users.noreply.github.com>
2022-11-16 21:09:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,38 +13,50 @@ let btnStatus = input.oninput = function(){
 
 btn.addEventListener('click', addTodoHandler);
 
-const tasks = [];
+let tasks = [];
+
 function addTodoHandler() {
 	let inputValue = input.value;
 	input.value = '';
 
 	tasks.push(inputValue);
 	
-	addTask();
+	addAndDisplayTask();
 	btnStatus();
-	
+	delTask();
+	localStorage.setItem('task', JSON.stringify(tasks));
 }
 
-function addTask() {
+function addAndDisplayTask() {
 	let value = '';
 	for (const task of tasks) {
 		value = task;
 	}
 	res.innerHTML += `
-  <li id="li">
-    <span class="icon-bin"></span>
-    <a class="input__link" href="#" id="inp">${value}
-	  </a>
+  <li id="li" class="task">
+	<span id="spn">  
+    <a id="inp" class="input__link" href="#"><span class="icon-bin"></span> ${value}
+	  </a>		
+	</span>
   </li>
 `;
 taskStatus();
-}
 
+}
 function taskStatus(){
 	let input__links = document.querySelectorAll('.input__link');
 	for (let i = 0; i < input__links.length; i++) {
 		input__links[i].addEventListener('click', () => {
 			input__links[i].classList.toggle('active');
+		});
+	}
+}
+function delTask() {
+	let iconBin = document.querySelectorAll('.icon-bin');
+  let input__links = document.querySelectorAll('.input__link');
+for (let i = 0; i < iconBin.length; i++) {
+		iconBin[i].addEventListener('click', () => {
+			input__links[i].remove();
 		});
 	}
 }
